99d81367b598b3f38636393ff6b48267a731a162
Sebastian Garrido <segasi@gmail.com>
2020-03-23 23:50:38 -0600

    Script para actualizar datos de México en serie de tiempo

---
+++ 02_codigo/04_preparar_datos_series_de_tiempo.R
@@ -0,0 +1,34 @@
+# Este script es necesario porque México publica la actualización de sus datos a las 19 hrs. (CDMX), una hora después del corte del CSSE de Johns Hopkins
+
+### Cargar paquetes, definir setup y tema de gráficas ----
+source("02_codigo/00_paquetes_setup_tema.R") 
+
+### Cargar funciones ----
+source("02_codigo/01_funciones_limpieza_bd.R")
+
+### Importar datos ----
+datos <- 
+  read_csv(str_c("04_datos_generados/actualizacion_diaria_st/serie_de_tiempo_diaria_por_pais_2020_03_", 
+                 day(Sys.Date()), 
+                 ".csv"))
+
+### Actualizar datos de México ----
+datos <- 
+  datos %>% 
+  mutate(casos_confirmados = ifelse(pais == "México", lead(casos_confirmados), casos_confirmados),
+         muertes = ifelse(pais == "México", lead(muertes), muertes),
+         recuperados = ifelse(pais == "México", lead(recuperados), recuperados),
+         cambio_diario_casos = ifelse(pais == "México", lead(cambio_diario_casos), cambio_diario_casos),
+         en_tratamiento = ifelse(pais == "México", lead(en_tratamiento), en_tratamiento),
+         por_casos_conf_fallecieron = ifelse(pais == "México", lead(por_casos_conf_fallecieron), por_casos_conf_fallecieron)) %>% 
+  mutate(casos_confirmados = ifelse(fecha_corte == max(fecha_corte) & pais == "México", 367, casos_confirmados),
+         muertes = ifelse(fecha_corte == max(fecha_corte) & pais == "México", 4, muertes),
+         recuperados = ifelse(fecha_corte == max(fecha_corte) & pais == "México", 4, recuperados),
+         total_casos = ifelse(pais == "México", 367, total_casos), 
+         cambio_diario_casos = ifelse(pais == "México", casos_confirmados - lag(casos_confirmados), cambio_diario_casos),
+         en_tratamiento = ifelse(pais == "México", casos_confirmados - muertes - recuperados, en_tratamiento),
+         por_casos_conf_fallecieron = ifelse(pais == "México", round(muertes/casos_confirmados*100, 1), por_casos_conf_fallecieron)) 
+
+datos %>% 
+  filter(pais == "México") %>% 
+  tail()
