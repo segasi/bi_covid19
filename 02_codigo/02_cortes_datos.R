296a9b66f846f655a3c1e500a135465a250713c8
Sebastian Garrido <segasi@gmail.com>
2020-03-23 23:48:28 -0600

    Script para definir corte de los datos que aparecerán en las gráficas

---
+++ 02_codigo/02_cortes_datos.R
@@ -0,0 +1,9 @@
+### Definir texto que indique fecha y hora de corte de los datos en los subtitulos ----
+
+subtitulo_lineas <- str_c("Cifras a las 18:30 hrs. del ", 
+                          day(Sys.Date()), 
+                          " de marzo de 2020 (CDMX)")
+
+subtitulo_treemaps <-  str_c("Cifras a las 18:00 hrs. del ", 
+                             day(Sys.Date()), 
+                             " de marzo de 2020 (CDMX)")
